71dcd02d1ddd7133688d79f4d7bf5ae758e24176
Bosseyang <bosse_yang@hotmail.com>
2025-08-25 11:12:20 +0200

    Create CharactersList.tsx

---
+++ src/components/CharactersList.tsx
@@ -0,0 +1,11 @@
+import type { ReactElement } from "react";
+
+export const CharactersList = (): ReactElement => {
+  return (
+    <section id="characters-list">
+      <header>
+        <h1>Characters List</h1>
+      </header>
+    </section>
+  );
+};
