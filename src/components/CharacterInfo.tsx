666a64d175bd64d82cbaa77823049d1d7a0d1ceb
Bosseyang <bosse_yang@hotmail.com>
2025-08-25 11:12:18 +0200

    Create CharacterInfo.tsx

---
+++ src/components/CharacterInfo.tsx
@@ -0,0 +1,9 @@
+import type { ReactElement } from "react";
+
+export const CharacterInfo = (): ReactElement => {
+  return (
+    <section id="character-info">
+      <h1>Character Info</h1>
+    </section>
+  );
+};
