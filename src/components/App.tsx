eab790e3592c7062add3f6da04576f234a60cd4d
Bosseyang <bosse_yang@hotmail.com>
2025-08-25 12:01:53 +0200

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,21 @@
+import { useEffect, useState } from "react";
 import "../css/index.css";
 import { CharacterInfo } from "./CharacterInfo";
-import { CharactersList } from "./CharactersList";
+import { CharacterList } from "./CharacterList";
 import { Hero } from "./Hero";
+import { fetchCharacters } from "../api";
+import type { ICharacter } from "../types";
 
 export const App = () => {
+  const [characters, setCharacters] = useState<ICharacter[]>([]);
+  useEffect(() => {
+    fetchCharacters().then((data) => setCharacters(data))
+  })
   return (
     <>
-      <Hero></Hero>
+      <Hero />
       <main id="main-content">
-        <CharactersList />
+        <CharacterList characters={characters}/>
         <CharacterInfo />
       </main>
     </>
