a9c438a204c3721cf1667189fdd2fbadb1418e24
Bosseyang <bosse_yang@hotmail.com>
2025-09-27 20:12:45 +0200

    Create CharacterHomeworld.tsx

---
+++ src/components/CharacterHomeworld.tsx
@@ -0,0 +1,13 @@
+import type { ReactElement } from "react";
+import type { IHomeworld } from "../types";
+
+interface ICharacterDetailsProps {
+  homeworld: IHomeworld;
+}
+
+export const CharacterHomeworld = ({
+  homeworld,
+}: ICharacterDetailsProps): ReactElement => {
+  return (
+  );
+};
